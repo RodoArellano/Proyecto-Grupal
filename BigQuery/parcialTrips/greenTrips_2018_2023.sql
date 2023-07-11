79472a01ad7556c5939d23c6ed8c79f5effc868c
fran-as <francisco.albasaez@outlook.com>
2023-07-11 18:18:37 -0400

    actualizan tablas bigquery

---
+++ BigQuery/parcialTrips/greenTrips_2018_2023.sql
@@ -0,0 +1,51 @@
+CREATE OR REPLACE TABLE `nyc-taxis-and-carbon-emission.trip_records_clean.greenTrips_2013_2023`
+AS
+SELECT pickup_datetime, dropoff_datetime, passenger_count, trip_distance, payment_type, pickup_location_id, dropoff_location_id, 
+    CAST(total_amount AS NUMERIC) AS total_amount, CAST(tip_amount AS NUMERIC) AS tip_amount
+FROM `bigquery-public-data.new_york_taxi_trips.tlc_green_trips_2014`
+WHERE passenger_count > 0 AND trip_distance > 0 AND total_amount > 0 AND payment_type IN ('1', '2')
+UNION ALL
+SELECT pickup_datetime, dropoff_datetime, passenger_count, trip_distance, payment_type, pickup_location_id, dropoff_location_id, 
+    CAST(total_amount AS NUMERIC) AS total_amount, CAST(tip_amount AS NUMERIC) AS tip_amount
+FROM `bigquery-public-data.new_york_taxi_trips.tlc_green_trips_2015`
+WHERE passenger_count > 0 AND trip_distance > 0 AND total_amount > 0 AND payment_type IN ('1', '2')
+UNION ALL
+SELECT pickup_datetime, dropoff_datetime, passenger_count, trip_distance, payment_type, pickup_location_id, dropoff_location_id, 
+    CAST(total_amount AS NUMERIC) AS total_amount, CAST(tip_amount AS NUMERIC) AS tip_amount
+FROM `bigquery-public-data.new_york_taxi_trips.tlc_green_trips_2016`
+WHERE passenger_count > 0 AND trip_distance > 0 AND total_amount > 0 AND payment_type IN ('1', '2')
+UNION ALL
+SELECT pickup_datetime, dropoff_datetime, passenger_count, trip_distance, payment_type, pickup_location_id, dropoff_location_id, 
+    CAST(total_amount AS NUMERIC) AS total_amount, CAST(tip_amount AS NUMERIC) AS tip_amount
+FROM `bigquery-public-data.new_york_taxi_trips.tlc_green_trips_2017`
+WHERE passenger_count > 0 AND trip_distance > 0 AND total_amount > 0 AND payment_type IN ('1', '2')
+UNION ALL
+SELECT pickup_datetime, dropoff_datetime, passenger_count, trip_distance, payment_type, pickup_location_id, dropoff_location_id, 
+    CAST(total_amount AS NUMERIC) AS total_amount, CAST(tip_amount AS NUMERIC) AS tip_amount
+FROM `bigquery-public-data.new_york_taxi_trips.tlc_green_trips_2018`
+WHERE passenger_count > 0 AND trip_distance > 0 AND total_amount > 0 AND payment_type IN ('1', '2')
+UNION ALL
+SELECT pickup_datetime, dropoff_datetime, passenger_count, trip_distance, payment_type, pickup_location_id, dropoff_location_id, 
+    CAST(total_amount AS NUMERIC) AS total_amount, CAST(tip_amount AS NUMERIC) AS tip_amount
+FROM `bigquery-public-data.new_york_taxi_trips.tlc_green_trips_2019`
+WHERE passenger_count > 0 AND trip_distance > 0 AND total_amount > 0 AND payment_type IN ('1', '2')
+UNION ALL
+SELECT pickup_datetime, dropoff_datetime, passenger_count, trip_distance, payment_type, pickup_location_id, dropoff_location_id, 
+    CAST(total_amount AS NUMERIC) AS total_amount, CAST(tip_amount AS NUMERIC) AS tip_amount
+FROM `bigquery-public-data.new_york_taxi_trips.tlc_green_trips_2020`
+WHERE passenger_count > 0 AND trip_distance > 0 AND total_amount > 0 AND payment_type IN ('1', '2')
+UNION ALL
+SELECT pickup_datetime, dropoff_datetime, passenger_count, trip_distance, payment_type, pickup_location_id, dropoff_location_id, 
+    CAST(total_amount AS NUMERIC) AS total_amount, CAST(tip_amount AS NUMERIC) AS tip_amount
+FROM `bigquery-public-data.new_york_taxi_trips.tlc_green_trips_2021`
+WHERE passenger_count > 0 AND trip_distance > 0 AND total_amount > 0 AND payment_type IN ('1', '2')
+UNION ALL
+SELECT pickup_datetime, dropoff_datetime, passenger_count, trip_distance, payment_type, pickup_location_id, dropoff_location_id, 
+    CAST(total_amount AS NUMERIC) AS total_amount, CAST(tip_amount AS NUMERIC) AS tip_amount
+FROM `bigquery-public-data.new_york_taxi_trips.tlc_green_trips_2022`
+WHERE passenger_count > 0 AND trip_distance > 0 AND total_amount > 0 AND payment_type IN ('1', '2')
+UNION ALL
+SELECT pickup_datetime, dropoff_datetime, passenger_count, trip_distance, payment_type, pickup_location_id, dropoff_location_id, 
+    CAST(total_amount AS NUMERIC) AS total_amount, CAST(tip_amount AS NUMERIC) AS tip_amount
+FROM `bigquery-public-data.new_york_taxi_trips.tlc_green_trips_2023`
+WHERE passenger_count > 0 AND trip_distance > 0 AND total_amount > 0 AND payment_type IN ('1', '2')
